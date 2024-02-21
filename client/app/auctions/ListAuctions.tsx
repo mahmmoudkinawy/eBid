2924eb4ce9a056f6d2fc7c8d4f00925f81c476f2
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2024-02-21 21:09:28 +0200

    Added support for searching and added a component for the empty search

--- client/app/auctions/ListAuctions.tsx
+++ client/app/auctions/ListAuctions.tsx
@@ -9,6 +9,7 @@ import Filters from './Filters';
 import { useParamsStore } from '@/hooks/useParamsStore';
 import { shallow } from 'zustand/shallow';
 import queryString from 'query-string';
+import EmptyFilter from '../components/EmptyFilter';
 
 export default function ListAuctions() {
   const [data, setData] = useState<PagedResult<Auction>>();
@@ -17,6 +18,8 @@ export default function ListAuctions() {
       pageNumber: state.pageNumber,
       pageSize: state.pageSize,
       searchTerm: state.searchTerm,
+      orderBy: state.orderBy,
+      filterBy: state.filterBy,
     }),
     shallow
   );
@@ -38,19 +41,25 @@ export default function ListAuctions() {
   return (
     <>
       <Filters />
-      <div className='grid grid-cols-4 gap-6'>
-        {data &&
-          data.results.map((auction) => (
-            <AuctionCard auction={auction} key={auction.id} />
-          ))}
-      </div>
-      <div className='flex justify-center mt-4'>
-        <PageSelector
-          pageChanged={setPageNumber}
-          currentPage={params.pageNumber}
-          pageCount={data.pageCount}
-        />
-      </div>
+      {data.totalCount === 0 ? (
+        <EmptyFilter showReset />
+      ) : (
+        <>
+          <div className='grid grid-cols-4 gap-6'>
+            {data &&
+              data.results.map((auction) => (
+                <AuctionCard auction={auction} key={auction.id} />
+              ))}
+          </div>
+          <div className='flex justify-center mt-4'>
+            <PageSelector
+              pageChanged={setPageNumber}
+              currentPage={params.pageNumber}
+              pageCount={data.pageCount}
+            />
+          </div>
+        </>
+      )}
     </>
   );
 }
