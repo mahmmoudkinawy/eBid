495ab9e9d136d60416f85a8d9f44016082aee6a1
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2024-03-01 15:30:24 +0200

    Added support for auth

---
+++ client/app/actions/authActions.ts
@@ -0,0 +1,15 @@
+import { getServerSession } from 'next-auth';
+
+export async function getSession() {
+  return await getServerSession();
+}
+
+export async function getCurrentUser() {
+  try {
+    const session = await getSession();
+    if (!session) return null;
+    return session.user;
+  } catch (error) {
+    return null;
+  }
+}
